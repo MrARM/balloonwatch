f71240db2f61f18a45f0b66dba3f8d6e7d2c33b5
MrARM <jordan@thesmokinggnu.us>
2022-06-23 09:53:22 -0500

    Add RS-41 serial date code parsing (#3)

--- src/features/rs41_datecode.js
+++ src/features/rs41_datecode.js
@@ -1,3 +1,51 @@
 // RS41 datecode - decode the RS41 serial and get the manufacturing date.
 
 // Reference: https://www.vaisala.com/sites/default/files/documents/Vaisala%20Radiosonde%20RS41%20Serial%20Number.pdf
+
+// This table exists solely due to the fact Vaisala skipped letters.
+const rs41_datecode_table = {
+    'J': 2013,
+    'K': 2014,
+    'L': 2015,
+    'M': 2016,
+    'N': 2017,
+    'P': 2018,
+    'R': 2019,
+    'S': 2020,
+    'T': 2021,
+    'U': 2022,
+    'V': 2023, // This, and letters past here are unconfirmed and may be subject to change if Vaisala wants to go past 2027.
+    'W': 2024,
+    'X': 2025,
+    'Y': 2026,
+    'Z': 2027
+};
+
+// https://stackoverflow.com/questions/16590500/calculate-date-from-week-number-in-javascript
+const getDateOfISOWeek = (w, y) => {
+    const simple = new Date(y, 0, 1 + (w - 1) * 7);
+    const dow = simple.getDay();
+    const ISOweekStart = simple;
+    if (dow <= 4)
+        ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1);
+    else
+        ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
+    return ISOweekStart;
+};
+
+const resolveDate = serial => {
+    const year = rs41_datecode_table[serial.slice(0, 1)];
+    const week = Number(serial.slice(1,3));
+    const days = Number(serial.slice(3,4)) - 1; // 0: Monday, 6: Sunday
+
+    const dateObj = getDateOfISOWeek(week, year);
+
+    // Add days next
+    dateObj.setDate(dateObj.getDate() + days);
+
+    return dateObj;
+};
+
+module.exports = {
+    resolveDate
+};
--- tests/features/sondeTemplates.test.js
+++ tests/features/sondeTemplates.test.js
@@ -24,6 +24,7 @@ test('Create a usual sonde launch message with a DFM-17', () => {
            "type": "rich",
            "title": "DFM17 20041652 has launched",
            "description": "",
+           "fields": [],
            "color": 65535,
            "url": "https://sondehub.org/20041652"
        }
@@ -33,7 +34,6 @@ test('Create a usual sonde launch message with a DFM-17', () => {
 // Test an unusual sonde launch message (DFM-17)
 test('Create an unusual sonde launch message with a DFM-17', () => {
     const message =  sondeTemplates.unusual(sonde.dfm17);
-    console.log(JSON.stringify(message, null, 2));
     expect(message).toEqual(sondeTemplates.discordEmbedWrapper(
         "<@&980937123463069716>", // SecKC discord unusual role
         {
@@ -43,7 +43,7 @@ test('Create an unusual sonde launch message with a DFM-17', () => {
             "color": 65535,
             "fields": [
                 {
-                    "name": "DFM 20041652",
+                    "name": "DFM17 20041652",
                     "value": '\u200B'
                 },
                 {
@@ -69,3 +69,66 @@ test('Create an unusual sonde launch message with a DFM-17', () => {
     ));
 });
 
+// Test a usual sonde launch message (RS-41)
+test('Create a usual sonde launch message with a RS-41', () => {
+    const message =  sondeTemplates.normal(sonde.rs41);
+    expect(message).toEqual(sondeTemplates.discordEmbedWrapper(
+        "<@&980936900204441630>", // SecKC discord usual role
+        {
+            "type": "rich",
+            "title": "RS41-SG S2430678 has launched",
+            "description": "",
+            "fields": [
+                 {
+                    "name": "Sonde manufactured: 6/10/2020",
+                    "value": '\u200B'
+                 }
+            ],
+            "color": 65535,
+            "url": "https://sondehub.org/S2430678"
+        }
+    ));
+ });
+ 
+ // Test an unusual sonde launch message (DFM-17)
+ test('Create an unusual sonde launch message with a RS41', () => {
+     const message =  sondeTemplates.unusual(sonde.rs41);
+     expect(message).toEqual(sondeTemplates.discordEmbedWrapper(
+         "<@&980937123463069716>", // SecKC discord unusual role
+         {
+             "type": "rich",
+             "title": "Unusual Sonde launch detected!",
+             "description": "",
+             "color": 65535,
+             "fields": [
+                 {
+                     "name": "RS41-SG S2430678",
+                     "value": '\u200B'
+                 },
+                 {
+                     "name": "Frequency: 405.11 MHz",
+                     "value": '\u200B'
+                 },
+                 {
+                     "name": "Position: 34.9396, 32.8633",
+                     "value": '\u200B'
+                 },
+                 {
+                     "name": "Altitude: 12345.6 m",
+                     "value": '\u200B'
+                 },
+                 {
+                    "name": "Sonde manufactured: 6/10/2020",
+                    "value": '\u200B'
+                 }
+             ],
+             "image": {
+                 "url": "https://i.imgur.com/T7goLBv.png",
+                 "height": 0,
+                 "width": 0
+             },
+             "url": "https://sondehub.org/S2430678"
+         }
+     ));
+ });
+ 
