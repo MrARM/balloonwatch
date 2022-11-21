3f976ac81e7eb02bd53fae02db3d1e97e47b7aa5
MrARM <jordan@themsmokinggnu.us>
2022-11-21 13:21:35 -0700

    Update node packages to prepare for new features

---
+++ src/features/chaseCarAlerts.js
@@ -0,0 +1,6 @@
+// Chase Car Alerts - Chase car notifications, if a chase car appears in the detection area, it will be indicated.
+// I’m debating how I’d want this to work though,
+// because I think sending an embed message is a bit excessive, especially if somebody was just testing their chase rig.
+// Another idea is to attach some embed field like 🚘 Chasers: ABC123_CHASE(maybe omit the chase part)
+
+// If no sondes are active, no notifications are generated.
